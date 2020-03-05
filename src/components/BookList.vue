b01aa1eb49adf4d6e55af95cd652bf1c6aa03a33
Daniel West <daniel.west@slalom.com>
2020-03-05 14:22:05 -0700

    book form added

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <book-item v-for="book in books" :book="book"></book-item>
+      <book-item v-for="book in books" :book="book" />
     </ul>
+    <book-form @addBook="appendBook" />
   </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 
 export default {
   name: "BookList",
@@ -23,7 +25,12 @@ export default {
     };
   },
   components: {
-    BookItem
+    BookItem, BookForm
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    }
   }
 };
 </script>
